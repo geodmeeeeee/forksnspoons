bc1d1993a95a7fa0ef425e76695de7931e07cefb
geodome21 <gibson.h.matt@gmail.com>
2026-03-04 17:32:25 +0000

    Fah Handshakes

--- index.js
+++ index.js
@@ -95,6 +95,7 @@ const games=[
 {title:"Grand Theft Auto",img:"images/grand theft auto.png",url:"games/Grand Theft Auto/index.html",cat:"shooter,driving,open world"},
 {title:"Grand Theft Auto Vice City",img:"images/grand theft auto vice city.png",url:"games/grand theft auto vice city/index.html",cat:"shooter,driving,open world"},
 {title:"Half Life",img:"images/Half Life.png",url:"games/Half Life/index.html",cat:"shooter"},
+{title:"Handshakes",img:"images/Handshakes.png",url:"games/Handshakes/index.html",cat:"puzzle"},
 {title:"Highway Racer",img:"images/Highway Racer.png",url:"games/highway racer/index.html",cat:"driving"},
 {title:"Hollow Kight",img:"images/Hollow Knight.png",url:"games/Hollow Knight/index.html",cat:"platformer"},
 {title:"Ice Dodo",img:"images/Icy Dodo.png",url:"games/Icy Dodo/index.html",cat:"platformer"},
