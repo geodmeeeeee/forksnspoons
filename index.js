5873df5d2dc7a544c94b6b120dbc5a8a9300b820
geodome21 <gibson.h.matt@gmail.com>
2026-03-10 16:25:34 +0000

    i wish i had the real agar.io

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ const games=[
 {title:"2 minute football",img:"images/2 minute football.png",url:"games/2 minute football/index.html",cat:"sports"},
 {title:"3Dash",img:"images/3Dash.png",url:"games/3Dash/index.html",cat:"rhythm"},
 {title:"12 Mini Battles",img:"images/12 Mini Battles.png",url:"games/12 Mini Battles/index.html",cat:"rhythm"},
+{title:"Agar.io Lite",img:"images/Agar.io Lite.png",url:"games/Agar.io Lite/index.html",cat:"rhythm"},
 {title:"Age of War",img:"images/Age of War.png",url:"games/Age of War/index.html",cat:"strategy"},
 {title:"Age of War 2",img:"images/Age of War 2.png",url:"games/Age of War 2/index.html",cat:"strategy"},
 {title:"Apple Worm",img:"images/Apple Worm.png",url:"games/Apple Worm/index.html",cat:"puzzle"},
