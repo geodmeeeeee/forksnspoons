df34ac252588b82fdf878145778eab8b038d699f
geodome21 <gibson.h.matt@gmail.com>
2026-03-17 19:30:40 +0000

    why the fuck does this shit exist i dont even know

--- index.js
+++ index.js
@@ -85,6 +85,7 @@ const games=[
 {title:"Core Ball",img:"images/Core Ball.png",url:"games/Core Ball/index.html",cat:"all"},
 {title:"Countmaster Stickman Games",img:"images/Countmaster Stickman Games.png",url:"games/Countmaster stickman games/index.html",cat:"all"},
 {title:"Crazy Cattle 3D",img:"images/Crazy Cattle 3D.png",url:"games/Crazy Cattle 3D/index.html",cat:"all"},
+{title:"CS GO clicker",img:"images/CS GO clicker.png",url:"games/CS GO clicker/index.html",cat:"all"},
 {title:"CupHead",img:"images/CupHead.png",url:"games/CupHead/index.html",cat:"platformer"},
 {title:"Dad N Me",img:"images/Dad n Me.png",url:"games/Dad N me/index.html",cat:"platformer"},
 {title:"Deltarune",img:"images/Deltarune.png",url:"games/Deltarune/index.html",cat:"all"},
