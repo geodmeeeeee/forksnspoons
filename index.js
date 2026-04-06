a80c01eee1843bee8149b9cc3c45624181b501dc
geodome21 <gibson.h.matt@gmail.com>
2026-04-06 21:02:57 +0000

    MUAHHAHHAHAHAAHAHAHA MR REVEAL I WILL PLAY SHOOTER GAMES IF I WANT

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ const games=[
 {title:"Tag",img:"images/Tag.png",url:"games/Tag/index.html",cat:"all"},
 {title:"Tanuki Sunset",img:"images/tanuki sunset.jpg",url:"games/Tanuki Sunset/index.html",cat:"all"},
 {title:"Time Shooter 2",img:"images/Time Shooter 2.jpg",url:"games/Time Shooter 2/index.html",cat:"all"},
+{title:"Time Shooter 3",img:"images/Time Shooter 3.png",url:"games/Time Shooter 3/index.html",cat:"all"},
 {title:"Ultrakill",img:"images/Ultrakill.png",url:"games/ultrakill/index.html",cat:"all"},
 {title:"We Become What We Behold",img:"images/We Become What We Behold.png",url:"games/We Become What We Behold/index.html",cat:"all"},
 ]
